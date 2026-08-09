778079a51d9703d5abfe36cfad6bbcd48eb0b5ea
Космонавт <kosmonavt@MacBook-Air-Kosmonavt.local>
2026-08-09 15:32:45 +0700

    Add Telegram prepared report command

--- api/[[...slug]].js
+++ api/[[...slug]].js
@@ -22,6 +22,7 @@ const handlers = {
   "setup-qstash-reminder": () => require(path.join(handlersDir, "setup-qstash-reminder.js")),
   "freeroll-reminder-send": () => require(path.join(handlersDir, "freeroll-reminder-send.js")),
   "send-to-user": () => require(path.join(handlersDir, "send-to-user.js")),
+  "telegram-report-webhook": () => require(path.join(handlersDir, "telegram-report-webhook.js")),
   "send-bulk": () => require(path.join(handlersDir, "send-bulk.js")),
   "cron-reminder-10min": () => require(path.join(handlersDir, "cron-reminder-10min.js")),
   "freeroll-reminder-subscribe": () => require(path.join(handlersDir, "freeroll-reminder-subscribe.js")),
